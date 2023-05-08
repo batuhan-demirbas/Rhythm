c7a41e9702dc658a492922cb2ae3f403226c8ecc
Batuhan Demirbaş <batuhan@demirbas.co>
2023-05-08 13:47:33 +0300

    init commit

---
+++ Rhythm/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rhythm
+//
+//  Created by Batuhan on 8.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
