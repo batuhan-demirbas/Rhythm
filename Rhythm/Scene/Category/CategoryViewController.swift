e914203d270fdc6b78827f29813ad909054da72c
Batuhan Demirbaş <batuhan@demirbas.co>
2023-05-10 23:28:23 +0300

    Added CategoryVC design to storyboard

--- Rhythm/Scene/Category/CategoryViewController.swift
+++ Rhythm/Scene/Category/CategoryViewController.swift
@@ -9,29 +9,64 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var viewModel: CategoryViewModel! {
         didSet {
             viewModel.delegate = self
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel.load()
+        
     }
-
+    
 }
 
 extension CategoryViewController: CategoryViewModelDelegate {
     func prepareCollectionView() {
-        //sfsdf
+        collectionView.register(UINib.loadNib(name: CategoryCollectionViewCell.reuseIdentifier), forCellWithReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier)
     }
     
     func reloadData() {
-        //sfsdf
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
     }
     
-    
-    
+}
+
+extension CategoryViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let viewModel = ArtistViewModel()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: "ArtistViewController") as? ArtistViewController else {
+            return
+        }
+        viewController.viewModel = viewModel
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfItems
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.reuseIdentifier, for: indexPath) as! CategoryCollectionViewCell
+        cell.artist = viewModel.artists[indexPath.row]
+        cell.configure()
+        return cell
+    }
+}
+
+extension CategoryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 48 - 16) / 2
+        let height = width
+        return CGSize(width: width, height: height)
+    }
 }
--- Rhythm/Scene/Category/Cell/CategoryCollectionViewCell.swift
+++ Rhythm/Scene/Category/Cell/CategoryCollectionViewCell.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+
+    static let reuseIdentifier: String = "CategoryCollectionViewCell"
+    var artist: ArtistsDatum?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        imageView.layer.borderColor = UIColor(named: "stroke.border")?.cgColor
+        imageView.layer.borderWidth = 1
+        imageView.addLinearGradient()
+        
     }
 
+    func configure() {
+        guard let artist = artist else { return }
+        imageView.loadImage(from: artist.pictureBig)
+        label.text = artist.name
+        
+    }
 }
