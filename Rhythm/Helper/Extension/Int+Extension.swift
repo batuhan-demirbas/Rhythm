7053cc806c90b9de6628725226bc21bb2338ee3c
Batuhan Demirbaş <batuhan@demirbas.co>
2023-05-12 11:26:05 +0300

    Int format extension added

--- Rhythm/Helper/Extension/Int+Extension.swift
+++ Rhythm/Helper/Extension/Int+Extension.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+extension Int {
+    func asFormattedString() -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = "."
+        formatter.groupingSize = 3
+        
+        if let formattedString = formatter.string(from: NSNumber(value: self)) {
+            return formattedString
+        } else {
+            return "\(self)"
+        }
+    }
+    
+    func asMinutesSeconds() -> String {
+        let minutes = self / 60
+        let seconds = self % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
+    
+}
